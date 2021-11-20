fd3382d43251a37441bd8614ff126f3d05185397
Sujee Maniyam <sujee@elephantscale.com>
2021-11-20 11:43:12 -0800

    updated java-dev container settings

--- start-java-dev.sh
+++ start-java-dev.sh
@@ -4,4 +4,15 @@
 export CURRENT_USER="$(id -u):$(id -g)"
 # echo $CURRENT_USER
 
-docker-compose run java-dev
+# make maven build dir, if doesn't exist
+mkdir -p  $HOME/.m2
+
+docker run -it --rm \
+    --user $CURRENT_USER \
+    -v $HOME/.m2:/var/maven/.m2  \
+    -v $(pwd)/config/maven/settings.xml:/usr/share/maven/conf/settings.xml:ro   \
+    -v $(pwd)/work:/work:z   \
+    -w /work \
+    maven:3-jdk-11  \
+    /bin/bash
+
