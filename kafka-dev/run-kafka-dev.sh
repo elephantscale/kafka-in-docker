7ab229b8c9e677f53e4c4605544942624f8b9b8e
Sujee Maniyam <sujee@elephantscale.com>
2022-07-22 01:09:13 -0700

    minor updates

--- kafka-dev/run-kafka-dev.sh
+++ kafka-dev/run-kafka-dev.sh
@@ -7,9 +7,13 @@ export CURRENT_USER="${CURRENT_USER-$(id -u):$(id -g)}"
 # make maven build dir, if doesn't exist
 mkdir -p  $HOME/.m2
 
+# just in case 
+docker network create bobafett-net 2> /dev/null
+
 docker run -it --rm \
     --user $CURRENT_USER \
     --network  bobafett-net \
+    --hostname kakfa-dev \
     -v $HOME/.m2:/var/maven/.m2  \
     -v $(pwd):/workspace:z   \
     -w /workspace \
